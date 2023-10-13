bcdb556e4e1fb0124a03daa04bcc10269e49cde5
Julian <43455717+Bluju@users.noreply.github.com>
2023-10-13 11:43:03 -0500

    Add files via upload

---
+++ dfsmSim.java
@@ -0,0 +1,17 @@
+import java.utill.file;
+import java.util.Scanner;
+//Team: Julian Sahagun, Diya Kafle
+public class dfsmSim{
+    //The program should
+    //1. Read the DFSM from the input file
+    //2. In a loop:
+        //a. ask the user to input a string which can be empty
+        //b. output "true" if the string is accepted by the machine, "false" otherwise
+    //3. Prompt the user to repeat step 2 or stop the program
+
+    public static void main(String[] args) {
+        //Read the DFSM
+        File file = new File("dfsm.txt");
+    }
+        
+}
